7310fd2966155cd17e3a314baa05bf30c7581ac0
ABBjaceng <43879993+ABBjaceng@users.noreply.github.com>
2021-02-15 20:19:43 +0100

    update

---
+++ 2d Projekt/Assets/MovementBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementBehaviour : MonoBehaviour
+{
+    public float speed;
+    public int yReset = 1;
+    public int xReset = 22;
+
+    void Update()
+    {
+        transform.Translate(Vector2.left * Time.deltaTime * speed);
+
+        if (transform.position.x <= -11)
+        {
+            transform.position = new Vector2(transform.position.x + xReset, transform.position.y - yReset);
+
+
+        }
+    }
+}
