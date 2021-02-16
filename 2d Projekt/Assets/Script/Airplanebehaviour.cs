99f9c217de11e151ca545729307e94fa7aa1dfdb
ABBjaceng <43879993+ABBjaceng@users.noreply.github.com>
2021-02-16 13:36:16 +0100

    bomb

---
+++ 2d Projekt/Assets/Script/Airplanebehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Airplanebehaviour : MonoBehaviour
+{
+    public GameObject bomb;
+
+    void Update()
+    {
+
+        // When spacebar is hit
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // instantiate the fireball object
+            Instantiate(bomb,
+                new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, 0),
+                new Quaternion(0, 0, 0, 0));
+        }
+    }
+
+}
---
+++ 2d Projekt/Assets/Script/HitBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitBehaviour : MonoBehaviour
+{
+
+    void Start()
+    {
+        Debug.Log("Tar Hit!");
+    }
+
+
+    void Update()
+    {
+        Debug.Log("Tar Hit2222!");
+    }
+
+
+    void onTriggerEnter2D(Collider2D col)
+    {Debug.Log("Target was Hit!" + col.gameObject.tag + this.gameObject.tag);
+        // When target is hit
+        if (col.gameObject.tag == "Bomb")
+        {
+            Debug.Log("Target was Hit!");
+
+            
+        }
+
+    }
+
+}
